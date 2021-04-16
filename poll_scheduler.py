4d9e917c2eb767c400cde3f29329d6b9c6b0f9c5
Joseph Xu <13371603724a@gmail.com>
2021-04-16 12:59:39 -0400

    update readme

--- poll_scheduler.py
+++ poll_scheduler.py
@@ -32,22 +32,3 @@ if (tag and group_id) or (not tag and not group_id):
     loop.run_until_complete(poll_scheduler(group_id, tag, duration))
 else:
     print("The arguments - group id and tag should appear at the same time!")
-
-#group_id = 586242376
-#group_id = 501560267
-
-# Morning Service Monday to Friday
-# 0,30 4 * * 1-5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='send'
-# 0 6 * * 1-5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='remind'
-# 30 6 * * 1-5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='stop'
-
-# # Wednesday Service and Friday Prayer Meeting
-# 30,50 17 * * 3,5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='send'
-# 30 20 * * 3,5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='remind'
-# 0 21 * * 3,5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='stop'
-
-# # Sunday Service
-# 0,30 8 * * 0 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='send'
-# 0 11 * * 0 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='remind'
-# 30 11 * * 0 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='stop'
-
--- readme.txt
+++ readme.txt
@@ -15,3 +15,22 @@ Sunday Serivce
 START 8:00Am
 STOP 11:30Am
 REMIND|PIN  8:00Am
+
+
+crontab -l
+
+
+# Morning Service Monday to Friday
+0,30 4 * * 1-5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='send'
+0 6 * * 1-5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='remind'
+30 6 * * 1-5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='stop'
+#
+# Wednesday Service and Friday Prayer Meeting
+0,30 17 * * 3,5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='send'
+0 20 * * 3,5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='remind'
+30 20 * * 3,5 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='stop'
+#
+# Sunday Service
+0,30 8 * * 0 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='send'
+0 11 * * 0 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='remind'
+30 11 * * 0 python3 ~/ScheduleServicePoll/poll_scheduler.py --group-id=586242376 --tag='stop'
